430819261223d1006b4353f7627c3d4cc1f86a8f
Gabosor <z.kdeqth1@gmail.com>
2024-06-05 18:43:18 -0400

    Corregido filament.

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,6 +5,7 @@ namespace App\Models;
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Filament\Panel;
 use Illuminate\Notifications\Notifiable;
+use Filament\Models\Contracts\FilamentUser;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
@@ -47,6 +48,6 @@ class User extends Authenticatable implements FilamentUser
     }
     public function canAccessPanel(Panel $panel): bool
     {
-        return str_ends_with($this->email, '@cinema.com') && $this->hasVerifiedEmail();
+        return str_ends_with($this->email,'@cinema.com') && $this->hasVerifiedEmail();
     }
 }
